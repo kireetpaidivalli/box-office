18060becb8fc06cf0ef7f8fe6921d83aacda6d6d
kireet <kireetpaidivalli@gmail.com>
2022-06-24 22:28:56 +0530

    added layout mangement and created title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <div>this home page</div>;
+  return <MainPageLayout>this home page</MainPageLayout>;
 };
 
 export default Home;
